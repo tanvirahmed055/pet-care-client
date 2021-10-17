8cda2c79450059b1f14f64e7a52d48d5b30600aa
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-18 02:18:58 +0600

    created useService custom hook

---
+++ src/hooks/useServices.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+const useServices = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('./services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    return [services];
+}
+
+export default useServices;

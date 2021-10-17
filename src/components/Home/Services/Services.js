e998d46396a2732926ddb8d68bda4a5d28084041
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-18 02:03:41 +0600

    added fake json data

---
+++ src/components/Home/Services/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Our Services</h1>
+            <p className="text-center"> Pets</p>
+        </div>
+    );
+};
+
+export default Services;

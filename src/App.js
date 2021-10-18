f3330b6fb390cd10be9def8cb408a6aab431bf5d
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-18 17:14:17 +0600

    did route setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home/Home/Home';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/services/:serviceId">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }

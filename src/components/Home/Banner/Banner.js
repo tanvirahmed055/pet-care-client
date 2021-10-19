83c38d2532831d7d911662b37f5bf4c244b6a141
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-19 22:36:28 +0600

    added Hash Link to a button in the banner section

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import './Banner.css';
+import { HashLink } from 'react-router-hash-link';
 
 const Banner = () => {
     return (
@@ -8,7 +9,8 @@ const Banner = () => {
             <div className="container centered">
                 <h1 className="fw-bolder text-center text-white ">Welcome to Pet Care</h1>
                 <h4 className="text-center text-white  mt-3">We are offering quality medical care with the latest medical<br /> technology for your pet friend.</h4>
-                <Button variant="primary" size="lg" className="mt-3">Know More!</Button>{' '}
+                <HashLink to="/home#services">
+                    <Button variant="primary" size="lg" className="mt-3">Know More!</Button></HashLink>
             </div>
         </div>
     );
